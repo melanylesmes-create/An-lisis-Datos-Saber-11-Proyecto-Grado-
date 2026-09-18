95dc35ebfe3f28f2950e939764b6b8c358b40b64
melany lesmes <melany.lemes@unimayor.edu.co>
2026-09-18 00:04:24 -0500

    feat (cambios peques)

--- Analisis-datos-Saber-11/backend2/backend2/settings.py
+++ Analisis-datos-Saber-11/backend2/backend2/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inicioSesion', 'instituciones', 'subirArchivos' #'resultados'
+    'inicioSesion', 'instituciones', 'subirArchivos','resultados'
 ]
 
 MIDDLEWARE = [
